33ece0e57bb9037c0a4292829fe6b43b234cf7f0
rks507469 <44531949+rks507469@users.noreply.github.com>
2020-10-05 22:09:42 +0530

    day1 solution

--- day1/6.cpp
+++ day1/6.cpp
@@ -1,1 +1,11 @@
 //6. What is the difference between including header file within angular bracket <> and double quotes ""
+/*
+By using the algular bracket that means we are instructing compiler to look for library in the default directory of libraries.
+But if we are using the quotes that means it will instruct compiler to look for library in current directory.
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+    return 0;
+}
